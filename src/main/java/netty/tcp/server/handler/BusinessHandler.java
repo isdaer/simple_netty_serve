51a545466b8986208972b5e00b96b1c29d306e9c
chenhui <chenhui@zeaho.com>
2020-06-22 17:36:06 +0800

    first commit

--- src/main/java/netty/tcp/server/handler/BusinessHandler.java
+++ src/main/java/netty/tcp/server/handler/BusinessHandler.java
@@ -1,13 +1,10 @@
 package netty.tcp.server.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import org.apache.log4j.Logger;
-import netty.tcp.utils.ByteUtils;
 
 public class BusinessHandler extends ChannelInboundHandlerAdapter {
-    private ObjectMapper objectMapper = ByteUtils.InstanceObjectMapper();
     private Logger logger = Logger.getLogger(this.getClass());
 
     @Override
